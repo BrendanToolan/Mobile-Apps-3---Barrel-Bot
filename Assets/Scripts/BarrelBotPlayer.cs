6cee7a440b51303d983732287031ece6e35f24a0
BrendanToolan <btoolan73@gmail.com>
2020-11-18 13:10:22 +0000

    Jump added to player script. Horizontal still needs some work

--- Assets/Scripts/BarrelBotPlayer.cs
+++ Assets/Scripts/BarrelBotPlayer.cs
@@ -5,21 +5,41 @@ using UnityEngine;
 public class BarrelBotPlayer : MonoBehaviour
 {
 
-    [SerializeField] float jumpSpeed = 5.0f;
-
+    [SerializeField] float jumpSpeed = 15.0f;
+    [SerializeField] float horizontalMovementSpeed = 15.0f;
 
     Rigidbody myRigidBody;
+
+    float gravityScaleAtStart;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody>();
+
+       // gravityScaleAtStart = myRigidBody.gravityScale;
     }
 
     // Update is called once per frame
     void Update()
     {
         Jump();
+    }
+
+    private void Movement()
+    {
+        float xDir = Input.GetAxis("Horizontal");
+        Vector3 playerVel = new Vector3(xDir*horizontalMovementSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVel;
+
+        bool playerCanMoveLeftandRight = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if(playerCanMoveLeftandRight)
+        {
+            transform.localScale = new Vector3(Mathf.Sign(myRigidBody.velocity.x), 15.0f);
+        }
+        else{
+
+        }
     }
 
     private void Jump(){
