db93e7c30a7217fa6b8b1facb15b7d81175bb32b
BrendanToolan <btoolan73@gmail.com>
2020-11-20 18:11:36 +0000

    Camera moves along the track, the track generates itself but after five clones disapears, will need to work on this issue

---
+++ Assets/Scripts/GameFlow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFlow : MonoBehaviour
+{
+
+    public Transform tile1Obj;
+    private Vector3 nextTileSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextTileSpawn.z = 30;
+        StartCoroutine(spawnTile());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator spawnTile(){
+        yield return new WaitForSeconds(1);
+        Instantiate(tile1Obj, nextTileSpawn, tile1Obj.rotation);
+        nextTileSpawn.z += 3;
+
+        StartCoroutine(spawnTile());
+    }
+
+}
