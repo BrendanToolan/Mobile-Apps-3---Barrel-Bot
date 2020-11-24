56a78362400b35058fd9b759eb1b926739bc5e34
BrendanToolan <btoolan73@gmail.com>
2020-11-24 19:46:37 +0000

    The player moves forward to the speed of the camera, player is able to move left and right but will keep moving to the choosen side without stopping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,18 @@ public class PlayerMovement : MonoBehaviour
     
     void Start()
     {
-        
+        GetComponent<Rigidbody>().velocity = new Vector3(0,0,3);
     }
 
-   
     void Update()
     {
-        
+        if(Input.GetKey("a"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(-1,0,3);
+        }
+        else if(Input.GetKey("d"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(1,0,3);
+        }
     }
 }
