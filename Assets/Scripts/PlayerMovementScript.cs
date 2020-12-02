d12b5aacdb5fb247065b71b59bc3449e04dccb00
BrendanToolan <btoolan73@gmail.com>
2020-12-02 20:16:21 +0000

    movement and jump added to player now, still need to work out how to move the player along with the camera

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -5,6 +5,7 @@ https://www.youtube.com/watch?v=Q4rtR8iNFbY
 https://www.youtube.com/watch?v=WPnX2qWzZvA
 https://www.codegrepper.com/code-examples/delphi/unity+3d+player+movement
 https://docs.unity3d.com/ScriptReference/CharacterController-isGrounded.html
+
 */
 
 using System.Collections;
@@ -14,22 +15,61 @@ using UnityEngine;
 public class PlayerMovementScript : MonoBehaviour
 {
 
-    CharacterController controller;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+
+    void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+/*
+    //CharacterController controller;
 
     public float jumpSpeed = 8.0f;
-    //public float gravity = 20.0f;
-    public float speed = 6.0f;
+    public float gravity = 20.0f;
+    public float speed = 3.0f;
 
     float gravityScaleAtStart;
 
-   // Rigidbody myRidBod;
+    Rigidbody myRidBod;
     //private Vector3 moveXDirection = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
 
+        myRidBod = GetComponent<Rigidbody>();
         GetComponent<Rigidbody>().velocity = new Vector3(0,0,3);
-     //   myRidBod = GetComponent<Rigidbody>();
+        
         
        // controller = GetComponent<CharacterController>();
     }
@@ -38,13 +78,14 @@ public class PlayerMovementScript : MonoBehaviour
     void Update()
     {
 
-       // Jump();
-        //XMovement();
+        Jump();
+        XMovement();
         //below code is make the player move to side to side and jump 
-      /*  var horizontal = Input.GetAxis("Horizontal");
+        //var horizontal = Input.GetAxis("Horizontal");
 
+        /*    
         transform.Translate(new Vector3(horizontal, 0) * (speed * Time.deltaTime));
-
+        
         if(controller.isGrounded)
         {
             moveXDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f);
@@ -58,11 +99,13 @@ public class PlayerMovementScript : MonoBehaviour
         }
 
         moveXDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveXDirection * Time.deltaTime);
-        */
-    }
+        controller.Move(moveXDirection * Time.deltaTime);*/
+        
+    //}
+    /*
 
-    /*private void XMovement()
+
+    private void XMovement()
     {
         float xDir = Input.GetAxis("Horizontal");
         Vector3 playerVel = new Vector3(xDir*speed, myRidBod.velocity.y);
@@ -78,7 +121,7 @@ public class PlayerMovementScript : MonoBehaviour
             Vector3 jumpVelToAdd = new Vector3(0f, jumpSpeed);
             myRidBod.velocity += jumpVelToAdd;
         }
-    }*/
-
+    }
+*/
   
 }
