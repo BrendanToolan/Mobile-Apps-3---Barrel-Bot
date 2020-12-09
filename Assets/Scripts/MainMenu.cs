2484359d7fdfd904bfad4bad2d7b41b2130e0464
BrendanToolan <btoolan73@gmail.com>
2020-12-09 16:31:59 +0000

    added quit button to menu, script for menu updated too

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,14 @@ public class MainMenu : MonoBehaviour
     //method that is used to start the game and put the player at the first level of the game
     public void StartGame()
     {
-        FindObjectOfType<GameSession>().ResetGameSession();
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+        Debug.Log("Starting The Game");
+    }
+
+    //method used to quit the game
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exiting the game");
     }
 }
