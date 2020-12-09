702bf1f17b273b560b05625b2fbce7d12ae2b46d
BrendanToolan <btoolan73@gmail.com>
2020-12-09 17:43:34 +0000

    PlayerMovement Script updated, player can move left and right more smoothly now

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -6,6 +6,7 @@ https://www.codegrepper.com/code-examples/delphi/unity+3d+player+movement
 https://docs.unity3d.com/ScriptReference/CharacterController-isGrounded.html
 https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
 https://www.youtube.com/watch?v=Ws1B1AGm-oM
+https://www.youtube.com/watch?v=chuJ_XIxbhk
 */
 
 using System.Collections;
@@ -14,86 +15,30 @@ using UnityEngine;
 
 public class PlayerMovementScript : MonoBehaviour
 {
-    Rigidbody myRidBod;
-    public float speed;
+    public Rigidbody myRidBod;
+    public float speed = 3;
 
-    //public Vector3 jump;
-    //public float jumpForce = 2.0f;
-    //public bool isGrounded;
+    float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody>().velocity = new Vector3(0,0,3);
-        //myRidBod = GetComponent<Rigidbody>();
-      //  jump = new Vector3(0.0f, 2.0f, 0.0f);
-       
+              
     }
 
-/*
-    void onCollisionStay()
+    void FixedUpdate()
     {
-        isGrounded = true;
+        Vector3 forwardMovement = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMovement = transform.right * horizontalInput * speed * Time.fixedDeltaTime;
+        myRidBod.MovePosition(myRidBod.position + forwardMovement + horizontalMovement);
     }
-
-    void onCollisionExit()
-    {
-        isGrounded = false;
-    }*/
 
     // Update is called once per frame
     void Update()
     {
-        xMovement();
-        //Jump();
-
-        /*if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            myRidBod.AddForce(jump*jumpForce, ForceMode.Impulse);
-            isGrounded = false;
-        }*/
+        horizontalInput = Input.GetAxis("Horizontal");
+      
     }
-
-    private void xMovement()
-    {
-        /*float horizontalM = Input.GetAxis("Horizontal");
-        float verticalM = Input.GetAxis("Vertical");
-        myRidBod.velocity = new Vector3 (horizontalM * speed, myRidBod.velocity.y, verticalM * speed);*/
-        if(Input.GetKey("a"))
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(-2.4f, 0, 3);
-            StartCoroutine(stopMove());
-        }
-
-        if(Input.GetKey("d"))
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(2.4f, 0, 3);
-            StartCoroutine(stopMove());
-        }
-
-        if(Input.GetKey("space"))
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 2, 3);
-            StartCoroutine(stopJump());
-        }
-    }
-
-    IEnumerator stopMove()
-    {
-        yield return new WaitForSeconds(0.5f);
-        GetComponent<Rigidbody>().velocity = new Vector3(0,0,3);
-    }
-
-    IEnumerator stopJump()
-    {
-        yield return new WaitForSeconds(0.5f);
-        GetComponent<Rigidbody>().velocity = new Vector3(0, -3, 3);
-        yield return new WaitForSeconds(0.5f);
-        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 3);
-
-    }
-
-   
 
     
 }
